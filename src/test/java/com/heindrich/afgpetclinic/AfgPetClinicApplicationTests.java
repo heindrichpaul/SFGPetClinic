f0837eac7c02f1b5adb9cb0e218d46439163783f
HeiPaul <hmV3URGG8CkeyA9!NEiU>
2020-01-20 12:33:23 +0100

    Initial Commit

---
+++ src/test/java/com/heindrich/afgpetclinic/AfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heindrich.afgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
