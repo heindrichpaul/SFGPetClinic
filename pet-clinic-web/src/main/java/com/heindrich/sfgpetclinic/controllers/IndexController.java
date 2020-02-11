dbb6216551a9434808f27cfbec587759ed31754f
Heindrich Paul <heindrich.paul@gmail.com>
2020-02-11 14:12:53 +0100

    Added index controller and index page closes #8

---
+++ pet-clinic-web/src/main/java/com/heindrich/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.heindrich.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by heindrichpaul on 11/02/2020
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", "", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
