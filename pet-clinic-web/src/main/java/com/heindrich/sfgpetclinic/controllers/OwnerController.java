ffbc2dccd8b7f507f9bf4220dac4776945a3ee78
Heindrich Paul <heindrich.paul@gmail.com>
2020-02-11 16:51:48 +0100

    Listing all owners and vets to their respective index pages .closes #16 and #15

--- pet-clinic-web/src/main/java/com/heindrich/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/heindrich/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.heindrich.sfgpetclinic.controllers;
 
+import com.heindrich.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/heindrich/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/heindrich/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.heindrich.sfgpetclinic.controllers;
 
+import com.heindrich.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
