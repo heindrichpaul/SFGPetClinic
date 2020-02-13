c21b4fc63f93be2b0962132c7f72431279d34891
Heindrich Paul <heindrich.paul@gmail.com>
2020-02-13 15:52:38 +0100

    Implement base entity. Closes #35

--- pet-clinic-data/src/main/java/com/heindrich/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/heindrich/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,26 @@
 package com.heindrich.sfgpetclinic.model;
 
 
-import java.io.Serializable;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
 
     public Long getId() {
         return id;
@@ -12,5 +28,21 @@ public class BaseEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
     }
 }
