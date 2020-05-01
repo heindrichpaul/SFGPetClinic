0f69d86ba4a8ac224aa3fdfac2178c58af142d7b
Heindrich Paul <heindrich.paul@gmail.com>
2020-05-01 14:10:40 +0200

    Added views for Owner and added a displayOwners method to the OwnerController

--- pet-clinic-web/src/main/java/com/heindrich/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/heindrich/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.heindrich.sfgpetclinic.controllers;
 import com.heindrich.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by heindrichpaul on 11/02/2020
@@ -17,14 +20,21 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String index(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
-    @RequestMapping("/find")
+    @GetMapping("/find")
     public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
--- pet-clinic-web/src/test/java/com/heindrich/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/heindrich/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,15 +24,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
+
     @Mock
-    OwnerService service;
+    OwnerService ownerService;
 
     @InjectMocks
     OwnerController controller;
 
     Set<Owner> owners;
 
-    MockMvc servlet;
+    MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
@@ -38,25 +41,29 @@ class OwnerControllerTest {
         owners.add(Owner.builder().id(1L).build());
         owners.add(Owner.builder().id(2L).build());
 
-        servlet = MockMvcBuilders.standaloneSetup(controller).build();
-    }
-
-    @Test
-    void listOwners() throws Exception {
-        when(service.findAll()).thenReturn(owners);
-
-        servlet.perform(get("/owners"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners", hasSize(2)));
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
     }
 
     @Test
     void findOwners() throws Exception {
-        servlet.perform(get("/owners/find"))
+        mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
 
-        verifyNoInteractions(service);
+        verifyNoInteractions(ownerService);
     }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
+    }
+
+
 }
